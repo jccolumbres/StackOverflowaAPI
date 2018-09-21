fb062a89fb0533c4bb79c90f3208f91821d65937
Jigs <jhegs.knightdev@gmail.com>
2018-09-21 12:46:36 +0800

    Finished setting up badge model

--- app/src/main/java/testapp/jccolumbres/stackoverflowapi/model/Badges.java
+++ app/src/main/java/testapp/jccolumbres/stackoverflowapi/model/Badges.java
@@ -1,4 +1,42 @@
 package testapp.jccolumbres.stackoverflowapi.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Badges {
+
+    @SerializedName("")
+    private Integer bronze;
+    private Integer silver;
+    private Integer gold;
+
+
+    public Badges(Integer bronze, Integer silver, Integer gold) {
+        this.setBronze(bronze);
+        this.setSilver(silver);
+        this.setGold(gold);
+    }
+
+    public Integer getBronze() {
+        return bronze;
+    }
+
+    public void setBronze(Integer bronze) {
+        this.bronze = bronze;
+    }
+
+    public Integer getSilver() {
+        return silver;
+    }
+
+    public void setSilver(Integer silver) {
+        this.silver = silver;
+    }
+
+    public Integer getGold() {
+        return gold;
+    }
+
+    public void setGold(Integer gold) {
+        this.gold = gold;
+    }
 }
