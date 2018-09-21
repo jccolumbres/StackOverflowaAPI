c48e0e8399a9fa7645fc211348249af35f325b1b
Jigs <jhegs.knightdev@gmail.com>
2018-09-21 16:13:32 +0800

    Set up models for UsersReceived

--- app/src/main/java/testapp/jccolumbres/stackoverflowapi/model/UsersReceived.java
+++ app/src/main/java/testapp/jccolumbres/stackoverflowapi/model/UsersReceived.java
@@ -1,7 +1,24 @@
 package testapp.jccolumbres.stackoverflowapi.model;
 
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class UsersReceived {
 
+    @SerializedName("items")
+    private List<TopUsers> topUsers;
 
+    public UsersReceived(List<TopUsers> topUsers) {
+        this.setTopUsers(topUsers);
+    }
+
+    public List<TopUsers> getTopUsers() {
+        return topUsers;
+    }
+
+    public void setTopUsers(List<TopUsers> topUsers) {
+        this.topUsers = topUsers;
+    }
 }
