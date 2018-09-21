b6f8ce46d73f85c8e0811a1e33fc5926f36cd192
Jigs <jhegs.knightdev@gmail.com>
2018-09-21 12:59:52 +0800

    Iniatialize Recyclerview, models and Adapter

--- app/src/main/java/testapp/jccolumbres/stackoverflowapi/activity/ListActivity.java
+++ app/src/main/java/testapp/jccolumbres/stackoverflowapi/activity/ListActivity.java
@@ -2,14 +2,30 @@ package testapp.jccolumbres.stackoverflowapi.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import testapp.jccolumbres.stackoverflowapi.R;
+import testapp.jccolumbres.stackoverflowapi.adapter.UserAdapter;
+import testapp.jccolumbres.stackoverflowapi.model.TopUsers;
 
 public class ListActivity extends AppCompatActivity {
+
+    RecyclerView myRecyclerView;
+    RecyclerView.Adapter myAdapter;
+    List<TopUsers> dataSource = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myRecyclerView = (RecyclerView) findViewById(R.id.rvTopUsers);
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        myAdapter = new UserAdapter(dataSource,R.layout.item_layout,getApplication());
+        myRecyclerView.setAdapter(myAdapter);
     }
 }
