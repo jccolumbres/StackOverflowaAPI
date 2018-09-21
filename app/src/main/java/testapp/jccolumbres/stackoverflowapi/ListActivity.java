695cb93410fa7699bfe132238e64f06dd37d1844
Jigs <jhegs.knightdev@gmail.com>
2018-09-21 10:32:59 +0800

    Renamed MainActivity to ListActivity

--- app/src/main/java/testapp/jccolumbres/stackoverflowapi/ListActivity.java
+++ app/src/main/java/testapp/jccolumbres/stackoverflowapi/ListActivity.java
@@ -3,7 +3,7 @@ package testapp.jccolumbres.stackoverflowapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
