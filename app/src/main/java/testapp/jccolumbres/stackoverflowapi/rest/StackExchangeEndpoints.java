38873be30a092bbae83de287a553d24cf452e3d3
Jigs <jhegs.knightdev@gmail.com>
2018-09-21 16:11:24 +0800

    modified Endpoints

--- app/src/main/java/testapp/jccolumbres/stackoverflowapi/rest/StackExchangeEndpoints.java
+++ app/src/main/java/testapp/jccolumbres/stackoverflowapi/rest/StackExchangeEndpoints.java
@@ -1,14 +1,12 @@
 package testapp.jccolumbres.stackoverflowapi.rest;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
-import testapp.jccolumbres.stackoverflowapi.model.TopUsers;
+import testapp.jccolumbres.stackoverflowapi.model.UsersReceived;
 
 public interface StackExchangeEndpoints {
 
 
     @GET("2.2/users?pagesize=5&order=desc&sort=reputation&site=stackoverflow")
-    Call<List<TopUsers>> getTopFiveUsers();
+    Call<UsersReceived> getUsers();
 }
