b040c4edfa66aaf76a9424f9476d3aaf2ec3bc98
Jigs <jhegs.knightdev@gmail.com>
2018-09-21 11:20:39 +0800

    set up Retrofit client

--- app/src/main/java/testapp/jccolumbres/stackoverflowapi/rest/APIClient.java
+++ app/src/main/java/testapp/jccolumbres/stackoverflowapi/rest/APIClient.java
@@ -1,4 +1,21 @@
 package testapp.jccolumbres.stackoverflowapi.rest;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class APIClient {
+    final static String BASE_URL = "https://api.stackexchange.com/";
+    public static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
 }
