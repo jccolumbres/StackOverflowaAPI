2eadd85557e074aeb948bcf70f1a9ffc6496c055
Jigs <jhegs.knightdev@gmail.com>
2018-09-21 11:25:07 +0800

    Added a GET call in interface file

--- app/src/main/java/testapp/jccolumbres/stackoverflowapi/rest/StackExchangeEndpoints.java
+++ app/src/main/java/testapp/jccolumbres/stackoverflowapi/rest/StackExchangeEndpoints.java
@@ -1,4 +1,14 @@
 package testapp.jccolumbres.stackoverflowapi.rest;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import testapp.jccolumbres.stackoverflowapi.model.TopUsers;
+
 public interface StackExchangeEndpoints {
+
+
+    @GET("2.2/users?pagesize=5&order=desc&sort=reputation&site=stackoverflow")
+    Call<List<TopUsers>> getTopFiveUsers();
 }
