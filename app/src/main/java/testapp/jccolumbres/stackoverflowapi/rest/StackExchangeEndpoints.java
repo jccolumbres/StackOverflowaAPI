5c5db08a393667e6b7a19c8524d5aaec9ae3802a
Jigs <jhegs.knightdev@gmail.com>
2018-09-24 11:13:47 +0800

    Added @Quert annotatioin

--- app/src/main/java/testapp/jccolumbres/stackoverflowapi/rest/StackExchangeEndpoints.java
+++ app/src/main/java/testapp/jccolumbres/stackoverflowapi/rest/StackExchangeEndpoints.java
@@ -2,11 +2,13 @@ package testapp.jccolumbres.stackoverflowapi.rest;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 import testapp.jccolumbres.stackoverflowapi.model.UsersReceived;
 
 public interface StackExchangeEndpoints {
 
 
-    @GET("2.2/users?pagesize=5&order=desc&sort=reputation&site=stackoverflow")
-    Call<UsersReceived> getUsers();
+    @GET("2.2/users?pagesize=5&order=desc&site=stackoverflow")
+    Call<UsersReceived> getUsers(@Query("sort") String sort);
 }
+//&sort=reputation
